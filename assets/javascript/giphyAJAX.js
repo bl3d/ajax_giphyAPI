ec9fd8ef9adac064a5f80dde3e9f3db6e3abc248
bl3d <mr.brentlewis@gmail.com>
2017-03-06 14:07:40 -0500

    initial files

---
+++ assets/javascript/giphyAJAX.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict'; 
+
+
+
+	//main giphy/ajax functionality
+	var giphy = {
+
+
+	   	init: function(){ 
+
+	   		console.log('ready to go');
+
+	   	}
+	};
+
+
+	// kick off giphy 
+	giphy.init(); 
+
+
+
+
+
+
+	//user events
+	//**************************************************//
+ 
+
+ 
+
+}()); 
