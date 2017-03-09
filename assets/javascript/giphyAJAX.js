ea761bfe6a5a46e4129014cf6c77cf12cb986b2b
bl3d <mr.brentlewis@gmail.com>
2017-03-08 22:30:16 -0500

    adding some styling to the page and elements

--- assets/javascript/giphyAJAX.js
+++ assets/javascript/giphyAJAX.js
@@ -6,18 +6,21 @@
 	//main giphy/ajax functionality
 	var giphy = {
 
-		topics: ["Myth Busters", "Game of Thrones", "House of Cards", "The Walking Dead", "Breaking Bad"],
+		//generate initial list of buttons for user
+		topics: ["Myth Busters", "Game of Thrones", "House of Cards", "The Walking Dead", "Breaking Bad", "Beavis and Butthead"],
 
 	   	init: function(){ 
 
+	   		//set buttons up on first load from array above
 	   		this.updateButtons();
 
 	   	},
 
 	   	runSearch: function(topic){ 
-	   		var topic = $(this).attr('data-topic')
+	   		//grab topic from btutton to us in search query below
+	   		var topic = $(this).attr('data-topic');
 	    	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-	        topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+	        topic + "&api_key=dc6zaTOxFJmzC&limit=10"; //limit to 10
 
 	        $.ajax({
 	        	url: queryURL,
@@ -26,19 +29,20 @@
 	        .done(function(response) {
 	            var results = response.data;
 
-	            $("#gifContent_holder").empty();
+	            //empty on each load as opposed to allowing them to stack up
+	            $("#gifContent_holder").html('<h3>( Click on an image to see it animate. )</h3>');
 
 	            for (var i = 0; i < results.length; i++) {
 	          		// console.log(results[i]);
-	            	var gifDiv = $('<div class="gifHolder">');
+	            	var gifDiv = $('<li class="gifHolder">');
 	            	var rating = results[i].rating;
 	            	var p = $("<p>").text("Rating: " + rating);
 	            	var gifImg = $("<img>");
 
 	            	gifImg
-	            	.attr('src', results[i].images.fixed_height_still.url)
-	            	.attr('data-still', results[i].images.fixed_height_still.url)
-	            	.attr('data-animate', results[i].images.fixed_height.url)
+	            	.attr('src', results[i].images.fixed_width_still.url)
+	            	.attr('data-still', results[i].images.fixed_width_still.url)
+	            	.attr('data-animate', results[i].images.fixed_width.url)
 	            	.attr('data-state', 'still')
 	            	.addClass('gif');
 
@@ -46,8 +50,19 @@
 	            	.append(gifImg)
 	            	.append(p); 
 
-	            	$("#gifContent_holder").append(gifDiv);	            
+	            	$("#gifContent_holder").append(gifDiv);
+
+	            	if (i === 0) {
+
+	            	};	            
 	            }
+
+	            //set background image to first gif result for selected TV show
+	            $('#bgGifImg').stop().animate({opacity: 0.0}, 350, function(){
+	            	$(this)
+	            	.css({backgroundImage: 'url("'+results[0].images.original_still.url+'")'})
+	            	.animate({opacity: 1.0}, 850);
+	            });	            
 	        });	   			   		
 	   	},
 
@@ -55,19 +70,27 @@
 	   		var thisGif = $(this); 
 
 	   		if (thisGif.attr('data-state') === 'still') {
-	   			thisGif
+	   			//if gif is currently still, switch it to play
+	   			thisGif	   			
 	   			.attr('src', thisGif.attr('data-animate'))
-	   			.attr('data-state', 'animate');
+	   			.attr('data-state', 'animate')
+	   			.parent().addClass('active');
 	   		}else{
-	   			thisGif
+	   			//else it is already animating, switch back to still
+	   			thisGif	   			
 	   			.attr('src', thisGif.attr('data-still'))
-	   			.attr('data-state', 'still');
+	   			.attr('data-state', 'still')
+	   			.parent().removeClass('active');
 	   		}
 	   	},
 
 	   	addButton: function(){ 
-	   		giphy.topics.push($('#filter-input').val().trim());
-	   		this.updateButtons();
+	   		var newShow = $('#filter-input').val().trim();
+	   		if (newShow) {
+		   		giphy.topics.push(newShow);
+		   		this.updateButtons();
+		   		$('#filter-input').val(''); //set value to blank for input	   			
+	   		}; 
 	   	},
 
 	   	updateButtons: function(){
